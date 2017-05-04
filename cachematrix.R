ba22e5b0a7be26ddc94db162f8933edbb7c04986
chamikaKity <chamika.subashinie@gmail.com>
2017-05-04 16:25:40 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,13 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(m = matrix()) {
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
 }
